6fec1bd2fe38d6ae0a4fa11734bcdaa2189818bf
Leonardo Cartaxo <leo.cartaxo@gmail.com>
2024-10-06 12:03:43 -0300

    added swagger

--- internal/user/API.go
+++ internal/user/API.go
@@ -19,6 +19,15 @@ func sendInternalServerError(w http.ResponseWriter) {
 	http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
 }
 
+// SaveUser godoc
+// @Summary      Save User
+// @Description  Save a user by giver form
+// @Tags         users
+// @Accept       json
+// @Produce      json
+// @Success      201  {object}  DTO
+// @Failure      500
+// @Router       /users [post]
 func (a *API) Save(w http.ResponseWriter, r *http.Request) {
 	createDTO := &CreateDTO{}
 	if err := json.NewDecoder(r.Body).Decode(createDTO); err != nil {
@@ -33,13 +42,20 @@ func (a *API) Save(w http.ResponseWriter, r *http.Request) {
 
 	render.Status(r, http.StatusCreated)
 	render.JSON(w, r, newUser)
-	//err = json.NewEncoder(w).Encode(newUser)
-	//if err != nil {
-	//	sendInternalServerError(w)
-	//	return
-	//}
 }
 
+// FindOneUser godoc
+// @Summary      Find one an User
+// @Description  Find one User by ID
+// @Tags         users
+// @Accept       json
+// @Produce      json
+// @Param        id   path      int  true  "User ID"
+// @Success      200  {object}  DTO
+// @Failure      400
+// @Failure      404
+// @Failure      500
+// @Router       /users/{id} [get]
 func (a *API) FindOne(w http.ResponseWriter, r *http.Request) {
 	id := chi.URLParam(r, "id")
 	user, err := a.service.FindOne(id)
@@ -52,6 +68,18 @@ func (a *API) FindOne(w http.ResponseWriter, r *http.Request) {
 	render.JSON(w, r, user)
 }
 
+// UpdateOneUser godoc
+// @Summary      Update one an User
+// @Description  Update one User by ID
+// @Tags         users
+// @Accept       json
+// @Produce      json
+// @Param        id   path      int  true  "User ID"
+// @Success      200  {object}  DTO
+// @Failure      400
+// @Failure      404
+// @Failure      500
+// @Router       /users/{id} [put]
 func (a *API) UpdateOne(w http.ResponseWriter, r *http.Request) {
 	id := chi.URLParam(r, "id")
 	updateDTO := &UpdateDTO{}
@@ -66,6 +94,17 @@ func (a *API) UpdateOne(w http.ResponseWriter, r *http.Request) {
 	render.JSON(w, r, user)
 }
 
+// FindOneUser godoc
+// @Summary      List all Users
+// @Description  List all User by ID
+// @Tags         users
+// @Accept       json
+// @Produce      json
+// @Success      200  {object}  []DTO
+// @Failure      400
+// @Failure      404
+// @Failure      500
+// @Router       /users [get]
 func (a *API) All(w http.ResponseWriter, r *http.Request) {
 	dtos, err := a.service.All()
 	if err != nil {
