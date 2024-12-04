08ea46f7b8cdace73efb1d3e3569226eb16ec25e
Leonardo Cartaxo <leo.cartaxo@gmail.com>
2024-12-03 22:52:18 -0300

    add user email and password and testing authentication middleware

--- internal/user/model.go
+++ internal/user/model.go
@@ -12,11 +12,11 @@ type Model struct {
 	ID        uuid.UUID `gorm:"type:uuid;default:gen_random_uuid()"`
 	CreatedAt time.Time
 	UpdatedAt time.Time
-	DeletedAt gorm.DeletedAt           `gorm:"index"`
-	Name      string                   `gorm:"uniqueIndex"`
+	DeletedAt gorm.DeletedAt `gorm:"index"`
+	Name      string
+	Email     string `gorm:"uniqueIndex"`
+	Password  string
 	Expenses  []shared.ExpenseRefModel `gorm:"foreignKey:UserID;references:ID"`
-	// uncomment this if you want the reverse relationship, but you have to change the target table to avoid cycle import
-	//Expenses  []expense.Model `gorm:"foreignKey:UserID;references:ID"`
 }
 
 type DTO struct {
@@ -36,6 +36,7 @@ type UpdateDTO struct {
 
 type FindOptions struct {
 	Name  string
+	Email string
 	Start string
 	End   string
 }
